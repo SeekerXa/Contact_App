e71bdc3bc81fa0466b40bb8454af738d566d2ba9
SeekerXa <tatpat02@gmail.com>
2023-06-15 18:00:45 +0200

    Auth routes. dashboard blade

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ContactNoteController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\WelcomeController;
@@ -25,29 +26,22 @@ use Illuminate\Validation\NestedRules;
 
 Route::get('/', WelcomeController::class);
 
-Route::resource('/contacts', ContactController::class);
-Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])->name('contacts.restore')->withTrashed();
-Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])->name('contacts.force-delete')->withTrashed();
-
-
-
-
-Route::resource('/companies', CompanyController::class);
-Route::resources([
-    '/tags' => TagController::class,
-    '/task' => TaskController::class
-]);
-Route::resource('/activities', ActivityController::class)->except([
-    'index',
-    'show'
-]);
-
-// Nested
-Route::resource('/contacts.notes', ContactNoteController::class)->shallow();
-
-Route::fallback(function () {
-    return "<h1>Sorry, the page does not exist</h1>";
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', DashboardController::class);
+    Route::resource('/contacts', ContactController::class);
+    Route::delete('/contacts/{contact}/restore', [ContactController::class, 'restore'])
+        ->name('contacts.restore')
+        ->withTrashed();
+    Route::delete('/contacts/{contact}/force-delete', [ContactController::class, 'forceDelete'])
+        ->name('contacts.force-delete')
+        ->withTrashed();
+    Route::resource('/companies', CompanyController::class);
+    Route::resources([
+        '/tags' => TagController::class,
+        '/tasks' => TaskController::class
+    ]);
+    Route::resource('/contacts.notes', ContactNoteController::class)->shallow();
+    Route::resource('/activities', ActivityController::class)->parameters([
+        'activities' => 'active'
+    ]);
 });
-
-
-
