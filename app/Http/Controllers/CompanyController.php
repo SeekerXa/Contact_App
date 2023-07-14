be91b3acbca50473638c2a48fe7dfeea49f8b229
SeekerXa <tatpat02@gmail.com>
2023-07-14 16:13:06 +0200

    delete and softdelete companies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -81,14 +81,29 @@ class CompanyController extends Controller
         return redirect()->route('companies.index')->with('message', 'Company has been updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+
+   
+    public function destroy(Company $company)
     {
-        //
+        $company->delete();
+        $redirect = request()->query('redirect');
+        return ($redirect ? redirect()->route($redirect) : back())
+            ->with('message', 'Company has been moved to trash.')
+            ->with('undoRoute', getUndoRoute('companies.restore', $company));
+    }
+
+    public function restore(Company $company)
+    {
+        $company->restore();
+        return back()
+            ->with('message', 'Company has been restored from trash.')
+            ->with('undoRoute', getUndoRoute('companies.destroy', $company));
+    }
+
+    public function forceDelete(Company $company)
+    {
+        $company->forceDelete();
+        return back()
+            ->with('message', 'Company has been removed permanently.');
     }
 }
--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,94 +1,80 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use App\Models\Contact;
-use App\Models\Company;
 use App\Http\Requests\ContactRequest;
+use App\Models\Company;
+use Illuminate\Http\Request;
+use App\Models\Contact;
+use Illuminate\Support\Facades\DB;
 
-
-class ContactController extends Controller 
+class ContactController extends Controller
 {
-    
-
     protected function userCompanies()
     {
         return Company::forUser(auth()->user())->orderBy('name')->pluck('name', 'id');
     }
 
-
     public function index()
     {
-      
         $companies = $this->userCompanies();
-       
+
         $contacts = Contact::allowedTrash()
-        ->allowedSorts(['first_name', 'last_name', 'email'], "-id")
-        ->allowedFilters('company_id')
-        ->allowedSearch('first_name', 'last_name', 'email')
-        ->forUser(auth()->user())
-        ->paginate(10);
-       
+            ->allowedSorts(['first_name', 'last_name', 'email'], "-id")
+            ->allowedFilters('company_id')
+            ->allowedSearch('first_name', 'last_name', 'email')
+            ->forUser(auth()->user())
+            ->paginate(10);
+
         return view('contacts.index', compact('contacts', 'companies'));
     }
-    
+
+    public function create()
+    {
+        $companies = $this->userCompanies();
+        $contact = new Contact();
+
+        return view('contacts.create', compact('companies', 'contact'));
+    }
+
     public function store(ContactRequest $request)
     {
         $request->user()->contacts()->create($request->all());
-        
-        return redirect()->route('contacts.index')->with('message','Contact has been added successfully');
+        return redirect()->route('contacts.index')->with('message', 'Contact has been added successfully');
     }
 
- 
-    public function create(){
-        $companies = $this->userCompanies();
-        $contact = new Contact();
-        
-        return view('contacts.create', compact('companies','contact') );
-    }
-
-    public function show(Contact $contact){
-
+    public function show(Contact $contact)
+    {
         return view('contacts.show')->with('contact', $contact);
     }
 
-    public function edit(Contact $contact){
+    public function edit(Contact $contact)
+    {
         $companies = $this->userCompanies();
-
-        return view('contacts.edit', compact('companies', 'contact') );
+        return view('contacts.edit', compact('companies', 'contact'));
     }
 
-   
-    public function update(ContactRequest $request, Contact $contact){
-
+    public function update(ContactRequest $request, Contact $contact)
+    {
         $contact->update($request->all());
-
-        return redirect()->route('contacts.index')->with('message','Contact has been updated successfully');
+        return redirect()->route('contacts.index')->with('message', 'Contact has been updated successfully');
     }
 
-    public function destroy(Contact $contact){
+    public function destroy(Contact $contact)
+    {
         $contact->delete();
         $redirect = request()->query('redirect');
-
         return ($redirect ? redirect()->route($redirect) : back())
             ->with('message', 'Contact has been moved to trash.')
-            ->with('undoRoute', $this->getUndoRoute('contacts.restore', $contact));
+            ->with('undoRoute', getUndoRoute('contacts.restore', $contact));
     }
-    
-    
 
     public function restore(Contact $contact)
     {
         $contact->restore();
-
         return back()
             ->with('message', 'Contact has been restored from trash.')
-            ->with('undoRoute', $this->getUndoRoute('contacts.destroy', $contact));
-    }
-    
-
-    protected function getUndoRoute($name, $resource)
-    {
-        return request()->missing('undo') ? route($name, [$resource->id, 'undo' => true]) : null;
+            ->with('undoRoute', getUndoRoute('contacts.destroy', $contact));
     }
 
     public function forceDelete(Contact $contact)
@@ -97,5 +83,4 @@ class ContactController extends Controller
         return back()
             ->with('message', 'Contact has been removed permanently.');
     }
-
 }
